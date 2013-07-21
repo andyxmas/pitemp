6ddb9bf92f772c5eba704cb0bda1c422a4814b4e
Andy Christmas <andy.christmas@gmail.com>
2013-07-21 12:29:47 +0100

    made some changes to how errors are handled. now just returns blank values in the relevant gdocs fields and puts an error or OK message in separate columns.

--- pitemp.py
+++ pitemp.py
@@ -13,22 +13,26 @@ import xml.etree.ElementTree as ET
 def get_temp():
     '''Get the temperature and humidity readings from the DHT22'''
     # Temp
-    output = subprocess.check_output(["/home/andy/python/bitbucket/pitemp/Adafruit_DHT",
-                                      "2302", "4"])
+    output = subprocess.check_output(
+        ["/home/andy/python/bitbucket/pitemp/Adafruit_DHT", "2302", "4"])
     matches = re.search("Temp =\s+([0-9.]+)", output)
     if (not matches):
-        time.sleep(3)
-        return "error"
-    temp = float(matches.group(1))
+        error_internal = "Error getting data from DHT22"
+        temp = ""
+    else:
+        temp = float(matches.group(1))
+        error_internal = "OK"
 
     # Humidity
     matches = re.search("Hum =\s+([0-9.]+)", output)
     if (not matches):
-        time.sleep(3)
-        return "ERROR"
-    humidity = float(matches.group(1))
+        error_internal = "Error getting data from DHT22"
+        humidity = ""
+    else:
+        humidity = float(matches.group(1))
+        error_internal = "OK"
 
-    return (temp, humidity)
+    return (temp, humidity, error_internal)
 
 
 def get_external_temp():
@@ -41,14 +45,17 @@ def get_external_temp():
         root = ET.fromstring(r.text)
         kelvin = float(root[1].attrib.get('value'))
         celcius = kelvin - 272.15
-        return celcius
+        error_external = "OK"
 
     except:
         print "had trouble gettin the temp from open weather map..."
-        return "error getting data"
+        celcius = ""
+        error_external = "error getting external temp"
+
+    return celcius, error_external
 
 
-def write_to_gdocs(temp, humidity, ext_temp):
+def write_to_gdocs(temp, humidity, ext_temp, err_int, err_ext):
     '''write our data to a google spreadsheet'''
     # Account details for google docs
     email = 'andy.christmas@gmail.com'
@@ -74,7 +81,9 @@ def write_to_gdocs(temp, humidity, ext_temp):
         values = [datetime.datetime.now(),
                   temp,
                   humidity,
-                  ext_temp]
+                  ext_temp,
+                  err_int,
+                  err_ext]
         worksheet.append_row(values)
 
         #success!
@@ -83,6 +92,9 @@ def write_to_gdocs(temp, humidity, ext_temp):
     except:
         print "Unable to append data.  Check your connection?"
 
-write_to_gdocs(get_external_temp(),
+write_to_gdocs(get_external_temp()[0],
                get_temp()[0],
-               get_temp()[1])
+               get_temp()[1],
+               get_temp()[2],
+               get_external_temp()[1]
+               )
